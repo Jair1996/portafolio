492c1828c78c1d30c0acf386975075dcd36b29e1
jair <jairdeyner730791@gmail.com>
2023-01-05 17:21:29 -0500

    Switalert agregado

--- src/app/modules/contact/components/contact-form/contact-form.component.ts
+++ src/app/modules/contact/components/contact-form/contact-form.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
@@ -21,9 +23,36 @@ export class ContactFormComponent {
 
   constructor(private fb: FormBuilder) {}
 
+  /**
+   * Retorna true si el campo es inválido y ha sido tocado
+   * @param control nombre del FormControl del FormGroup
+   * @returns boolean
+   */
   isTheFieldInvalid(control: string) {
     return (
-      this.contactForm.controls[control].errors && this.contactForm.controls[control].touched
+      this.contactForm.controls[control].errors &&
+      this.contactForm.controls[control].touched
     );
+  }
+
+  /**
+   * Activa el modal cons los datos enviados por el formulario
+   * y resetea el formulario
+   */
+  sendMessage() {
+    Swal.fire({
+      icon: 'success',
+      title: 'Mensaje enviado',
+      html: `
+      <p>Nombre: ${this.contactForm.get('name')?.value}</p>
+      <p>Correo: ${this.contactForm.get('email')?.value}</p>
+      <p>Mensaje: ${this.contactForm.get('message')?.value}</p>
+      `,
+      showConfirmButton: false,
+      showCancelButton: true,
+      cancelButtonText: 'Cerrar',
+    });
+
+    this.contactForm.reset();
   }
 }
